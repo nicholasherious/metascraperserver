5d2b5f0529db9208963356d7613ba33f97c64fb1
nherious <nherious@gmail.com>
2021-10-03 19:44:13 -0700

    serve updates

--- routes/posts.js
+++ routes/posts.js
@@ -7,15 +7,24 @@ const GetLinks =  require('../models/GetLinks')
 
 router.get('/', async (req, res) => {
   try {
-    const posts = await GetLinks.find()
+    const posts = await GetLinks.find().sort({_id:-1})
     res.json(posts)
   } catch (error) {
     res.json({message: error})
   }
 });
 
-router.post('/', scrapeUrl, (req, res) => {
-    res.send(req.scrapedLink)
+router.post('/', scrapeUrl, async (req, res) => {
+  const scraped = req.scrapedLink
+  const description = req.body.desc
+  const post = new GetLinks({...scraped, desc: description})
+
+    try {
+      const savedPost = await post.save()
+      res.json(savedPost)
+    } catch (error) {
+      res.json({ message: error})
+    }
 })
 
 router.get('/:postId', async (req, res) => {
